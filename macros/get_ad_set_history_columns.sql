8da3a6ddeefde76cb11d9a4ba5853fe5520d6e7a
mbm-pbc-reporting <pbc_reporting@mediabymother.com>
2024-09-09 09:52:18 +0530

    Update get_ad_set_history_columns.sql

--- macros/get_ad_set_history_columns.sql
+++ macros/get_ad_set_history_columns.sql
@@ -11,7 +11,8 @@
     {"name": "bid_strategy", "datatype": dbt.type_string()},
     {"name": "daily_budget", "datatype": dbt.type_int()},
     {"name": "budget_remaining", "datatype": dbt.type_int()},
-    {"name": "status", "datatype": dbt.type_string()}
+    {"name": "status", "datatype": dbt.type_string()},
+    {"name": "targeting_publisher_platforms", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
